4f03c250603efc2b7795c4e4a96b3dd27fe693e1
Aviva Shuman <97950104+Ashugirl@users.noreply.github.com>
2022-08-08 15:49:44 +0200

    Initial commit

---
+++ src/be/intecbrussel/Friend.java
@@ -0,0 +1,48 @@
+package be.intecbrussel;
+
+public class Friend {
+    private String name;
+    private boolean family;
+    private int yearsKnown;
+    private int friendShipLevel;
+
+
+    public Friend(String name, boolean family, int yearsKnown, int friendShipLevel) {
+        this.name = name;
+        this.family = family;
+        this.yearsKnown = yearsKnown;
+        this.friendShipLevel = friendShipLevel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isFamily() {
+        return family;
+    }
+
+    public void setFamily(boolean family) {
+        this.family = family;
+    }
+
+    public int getYearsKnown() {
+        return yearsKnown;
+    }
+
+    public void setYearsKnown(int yearsKnown) {
+        this.yearsKnown = yearsKnown;
+    }
+
+    public int getFriendShipLevel() {
+        return friendShipLevel;
+    }
+
+    public void setFriendShipLevel(int friendShipLevel) {
+        this.friendShipLevel = friendShipLevel;
+    }
+}
